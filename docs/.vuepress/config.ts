492e96561473796bedfec2bb53e3c9cbfa656b05
aier9500 <aierjjwu8@outlook.com>
2025-12-09 18:26:20 -0500

    Display last updated date.

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -59,7 +59,9 @@ export default defineUserConfig({
 
     /* 页内信息 */
     // editLink: true,
-    lastUpdated: true,
+    lastUpdated: {
+      formatOptions: { dateStyle: "long", timeStyle: "long" },
+    },
     contributors: {
       mode: "block",
     },
