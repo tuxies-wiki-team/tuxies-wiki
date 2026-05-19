049d9fd28c9007d31f369bea39756a84f8bbc081
aier9500 <aierjjwu8@outlook.com>
2026-05-18 20:44:37 -0400

    Cloudflare testing

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import { plumeTheme } from "vuepress-theme-plume";
 
 export default defineUserConfig({
-  base: "/",
+  base: process.env.VUEPRESS_BASE || "/",
   lang: "en-US",
   locales: {
     "/": {
